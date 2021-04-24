4a09f400637662efdcde0d28ff7356218d5ce3c3
TrilochanSahoo <71021905+TrilochanSahoo@users.noreply.github.com>
2021-04-24 22:53:59 +0530

    update
    updating the code

--- feature.js
+++ feature.js
@@ -1,16 +1,18 @@
-const image = document.querySelectorAll('.img');
+const box1 = document.querySelector('.box1')
+const box = document.querySelectorAll('.box');
 
 window.addEventListener('scroll',()=>{
-    
     let offset = window.pageYOffset;
     // console.log("this is first "+image.style.backgroundPositionY)
-    // image.style.backgroundPositionY =offset * 0.7 +"px";
+    box1.style.backgroundPositionY =offset * 0.7 +"px";
+    if(window.pageYOffset!=0){
+        box1.style.filter="grayscale(100%)";
+    }else{
+        box1.style.filter="grayscale(0%)";
+    }
     // console.log(image.style.backgroundPositionY)
-    image.forEach((container,i)=>{
-
+    box.forEach((container,i)=>{
+        console.log("offset value after="+offset)
         container.style.backgroundPositionY = (offset-container.offsetTop)*0.6+'px';
-        if(window.pageYOffset!=0){
-            container.style.filter="grayscale(100%)";
-        }
     })
 })
